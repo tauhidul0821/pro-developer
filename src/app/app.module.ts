46303eb236ea19d7d0fb8c7214668c21ce688523
tauhidbs23 <tauhidul.islam@brainstation23.com>
2022-12-26 23:09:32 +0600

    router working okay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { MessageComponent } from './message/message.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    MessageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
