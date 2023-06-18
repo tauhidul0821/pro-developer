a6f0b09377a4a9cb2cfceb36fed1e3d86366afa2
tauhidbs23 <tauhidul.islam@brainstation23.com>
2023-06-18 22:38:40 +0600

    error message are separed from component

---
+++ src/app/angu-form/components/error-message/error-message.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {FormGroup, FormGroupDirective} from '@angular/forms';
+
+@Component({
+  selector: 'app-error-message',
+  templateUrl: './error-message.component.html',
+  styleUrls: ['./error-message.component.scss']
+})
+export class ErrorMessageComponent implements OnInit {
+  form!: FormGroup;
+  @Input('controlName') controlName;
+
+  constructor(private rootFormGroup: FormGroupDirective) {}
+
+  ngOnInit(): void {
+    this.form = this.rootFormGroup.control;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import {AppRoutingModule} from '@app/app-routing.module';
 import {AppComponent} from '@app/app.component';
 import {MessageService} from '@services';
 import {ReactiveFormsModule} from '@angular/forms';
+import {ErrorMessageComponent} from './angu-form/components/error-message/error-message.component';
 
 @NgModule({
-  declarations: [AppComponent, AppRoutingModule.components],
+  declarations: [AppComponent, AppRoutingModule.components, ErrorMessageComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule],
   providers: [MessageService],
   bootstrap: [AppComponent]
