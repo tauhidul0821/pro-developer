9a2e37e1179c77807843821ef22a0355be3024eb
Md. Tauhidul Islam <tuhin2093@gmail.com>
2025-02-23 14:26:38 +0600

    custom directive working fine

--- src/app/custom-directive/custom-directive.component.ts
+++ src/app/custom-directive/custom-directive.component.ts
@@ -1,10 +1,34 @@
 import {Component, OnInit} from '@angular/core';
 import {HighlighDirective} from './highligh.directive';
+import {Observable, from, Subject} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-custom-directive',
-  imports: [HighlighDirective],
+  imports: [HighlighDirective, AsyncPipe],
   templateUrl: './custom-directive.component.html',
   styleUrl: './custom-directive.component.scss'
 })
-export class CustomDirectiveComponent {}
+export class CustomDirectiveComponent {
+  time: Observable<string>;
+  subject: Subject<number> = new Subject<number>();
+
+  source: Observable<number> = from([1, 2, 3, 4]);
+
+  constructor() {
+    this.time = new Observable((observer) => {
+      setInterval(() => observer.next(new Date().toString()), 1000);
+    });
+
+    this.subject.subscribe({
+      next: (v) => console.log(`observerA: ${v}`)
+    });
+
+    this.subject.subscribe({
+      next: (v) => console.log(`observerB: ${v}`)
+    });
+
+    this.subject.next(1);
+    this.subject.next(2);
+  }
+}
